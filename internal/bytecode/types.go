900cbab6dd89069bb253ea9b76dc33c22bfdc7f8
Sitnikov Artem <artemsito06@gmail.com>
2025-12-02 15:46:32 +0300

    add types 1.0

---
+++ internal/bytecode/types.go
@@ -0,0 +1,65 @@
+﻿package bytecode
+
+type TypeKind byte
+
+const (
+	TypeInvalid TypeKind = iota
+	TypeInt
+	TypeFloat
+	TypeBool
+	TypeString
+	TypeChar
+	TypeVoid
+	TypeNull
+)
+
+type ValueKind byte
+
+const (
+	ValInt ValueKind = iota
+	ValFloat
+	ValBool
+	ValString
+	ValChar
+	ValNull
+)
+
+type Value struct {
+	Kind ValueKind
+	I    int64
+	F    float64
+	B    bool
+	S    string
+	C    byte
+}
+
+type OpCode byte
+
+const (
+	OpConst OpCode = iota
+	OpLoadLocal
+	OpStoreLocal
+	OpAdd
+	OpSub
+	OpMul
+	OpDiv
+	OpMod
+	OpPow
+
+	OpEq
+	OpNe
+	OpLt
+	OpLe
+	OpGt
+	OpGe
+
+	OpNeg
+	OpNot
+
+	OpJump
+	OpJumpIfFalse
+	OpPop
+
+	OpCall
+	OpReturn
+)
