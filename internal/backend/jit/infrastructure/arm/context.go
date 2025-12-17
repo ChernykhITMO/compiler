3e8afac93857a33cfdc1df284cc54751030d93dd
Maxim Kaplenkov <mkkaplenkov@gmail.com>
2025-12-17 15:47:51 +0300

    feat: del proc

--- internal/backend/jit/infrastructure/arm/context.go
+++ internal/backend/jit/infrastructure/arm/context.go
@@ -2,13 +2,10 @@ package arm
 
 import "unsafe"
 
-// контракт между ВМ го и машинным кодом
-
 type ContextVM struct {
 	LocalsBase unsafe.Pointer
 	StackBase  unsafe.Pointer
 	StackSize  uint32
 	_          uint32 // чтобы следующая инструкция 8 байт начиналась с адреса кратного 8
-	NextIp     uint32
 	DidReturn  uint32
 }
